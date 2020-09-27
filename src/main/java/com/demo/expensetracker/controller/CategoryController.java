39a4c60ee1bce66e044b0400cf10c7443e0d85ce
Alexandru Oliva <olivaalexandru@yahoo.com>
2020-09-27 20:59:04 +0300

    Created post request for a new category

--- src/main/java/com/demo/expensetracker/controller/CategoryController.java
+++ src/main/java/com/demo/expensetracker/controller/CategoryController.java
@@ -5,11 +5,9 @@ import com.demo.expensetracker.model.Category;
 import com.demo.expensetracker.repository.CategoryRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URISyntaxException;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -38,6 +36,13 @@ public class CategoryController {
         Optional<Category> category = categoryRepository.findById(id);
         return category.map(response -> ResponseEntity.ok().body(response))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
+    }
+
+    @PostMapping("/category")
+    ResponseEntity<Category> createCategory(@Validated @RequestBody Category category) throws URISyntaxException{
+        Category result= categoryRepository.save(category);
+        return ResponseEntity.created(new URI("/api/category" + result.getId())).body(result);
 
     }
 
