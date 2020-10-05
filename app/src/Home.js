372f8365a9a98a70f90a255d3c3a07ca5cfad5c5
Alexandru Oliva <olivaalexandru@yahoo.com>
2020-10-06 00:10:32 +0300

    Implemented the 'Navigation bar' component for the 'Home' page

--- app/src/Home.js
+++ app/src/Home.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import AppNav from './AppNav';
 
 class Home extends Component {
     state = {  }
     render() { 
-        return ( <div>Welcome to the home page!</div> );
+        return ( <AppNav/>);
     }
 }
  
--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './Home'
-
+// import Category from './Category'
+import AppNav from './AppNav'
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 ReactDOM.render(
