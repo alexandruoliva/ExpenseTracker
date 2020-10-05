6c76d4ba3b01b6292f0d78200e9a6c94b15ea25e
Alexandru Oliva <olivaalexandru@yahoo.com>
2020-10-06 00:28:15 +0300

    Added the 'Expense' and 'App' component

---
+++ app/src/Expenses.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+class Expenses extends Component {
+    state = {  }
+    render() { 
+        return ( <h2>Expenses</h2> );
+    }
+}
+ 
+export default Expenses;
--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './Home'
-// import Category from './Category'
-import AppNav from './AppNav'
+import App from './App'
 import 'bootstrap/dist/css/bootstrap.css'
 
 
 ReactDOM.render(
-  <Home />,
+  <App />,
   document.getElementById('root')
 );
